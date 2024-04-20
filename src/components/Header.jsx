94dde40ce70b896c7584c25dfb0d7fbd6a5e8e3e
Burhan-Git <bu61796@gmail.com>
2024-04-20 22:27:03 +0500

    -m

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex felx-col mt-8 mb-16 items-center">
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
